42ac55c7a4ad0f7281c15621959aa1fd2fbcf4a8
Reynaldo Juan Pereira Heredia <darkdeymon04@gmail.com>
2018-04-29 20:09:12 -0400

    v1

---
+++ app/src/main/java/com/example/darkdeymon/mapsnormal/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.darkdeymon.mapsnormal;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.maps);
+        mapFragment.getMapAsync(this);
+
+        Button b = (Button)findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri gmmIntentUri = Uri.parse("google.navigation:q=-19.589177,-65.753558");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        LatLng potosi= new LatLng(-19.589177,-65.753558);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(potosi,16));
+        map.addMarker(new MarkerOptions().title("Postosi").snippet("Plaza 10 de Noviembre").position(potosi));
+    }
+}
